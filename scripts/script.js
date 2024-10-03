ae6d01102039735cea19e16b439970e968f4cce7
gbouldin43 <gbouldin43@gmail.com>
2024-10-03 11:24:55 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    let score = 0;
+
+    $('#player').draggable({
+        containment: '#game-area',
+        drag: function () {
+            checkCollision();
+        }
+    });
+
+    $('#reset-button').click(function () {
+        score = 0;
+        $('#score').text(score);
+        placeCoinRandomly(); 
+    });
+
+    placeCoinRandomly();
+
+    function placeCoinRandomly() {
+        let randomTop = Math.random() * 380;
+        let randomLeft = Math.random() * 380;
+        $('#coin').css({ top: randomTop + 'px', left: randomLeft + 'px' });
+    }
+
+    function checkCollision() {
+        let player = $('#player');
+        let coin = $('#coin');
+        let playerPos = player.position();
+        let coinPos = coin.position();
+
+        if (playerPos.left < coinPos.left + 20 &&
+            playerPos.left + 30 > coinPos.left &&
+            playerPos.top < coinPos.top + 20 &&
+            playerPos.top + 30 > coinPos.top) {
+            score++;
+            $('#score').text(score);
+
+            placeCoinRandomly();
+        }
+    }
+});
